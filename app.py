2b8dd4b282fd65eff779f0216d8381eaa08c2f52
mehranLotfi <rahahasani712@gmail.com>
2025-10-09 23:06:14 +0330

    fix: bind Flask to 0.0.0.0 for Render

--- app.py
+++ app.py
@@ -3319,6 +3319,5 @@ if __name__ == "__main__":
     with app.app_context():
         db.create_all()
 
-    # تنظیم پورت برای Render
     port = int(os.environ.get("PORT", 5000))
     app.run(host="0.0.0.0", port=port)
